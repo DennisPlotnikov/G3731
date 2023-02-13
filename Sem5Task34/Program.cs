df34fbeb7a1ba4f820b287c0621bd39d017caccb
Denis Plotnikov <denis.plotnikov.82@ya.ru>
2023-02-13 21:03:58 +0500

    Homework 34,36,38 added

--- Sem5Task34/Program.cs
+++ Sem5Task34/Program.cs
@@ -55,9 +55,31 @@ bool Test(int n)
     return ((n%2==0));
 }
 
+int[] BubbleSort(int[] arr)
+        {
+            int temp;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = i + 1; j < arr.Length; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[j];
+                        arr[j] = temp;
+                    }                   
+                }            
+            }
+            return arr;
+        }
+
+
+
 int[] arr = Gen1DArr(5, 100, 999);
 
 int result = CountElem(arr);
 
 Print1DArr(arr);
 PrintData("в массиве четных элементов:", result);
+BubbleSort(arr);
+Print1DArr(arr);
--- Sem5Task38/Program.cs
+++ Sem5Task38/Program.cs
@@ -6,6 +6,57 @@
 // разницу между первым и последним элементом. Для задачи со звездочкой
 // использовать заполнение массива целыми числами.
 
+// double[] Gen1DArr(int len, double b, double e)
+// {
+//     double[] arr = new double[len];
+
+//     Random gen = new Random();
+
+//     for (int i = 0; i < len; i++)
+//     {
+//         arr[i] = gen.NextDouble();
+//     }
+
+//     return arr;
+// }
+
+// void Print1DArr(double[] arr)
+// {
+//     Console.Write("[");
+//     for (int i = 0; i < arr.Length - 1; i++)
+//     {
+//         Console.Write(arr[i] + ",");
+//     }
+//     Console.WriteLine(arr[arr.Length - 1] + "]");
+// }
+
+// void PrintData(string msg1, double msg2)
+
+// {
+//     Console.WriteLine(msg1);
+//     Console.WriteLine(msg2);
+// }
+
+// double DiffElem(double [] arr)
+// {
+//     double res = 0;
+//     res = arr.Max() - arr.Min();
+//     return res;
+// }
+
+
+// double[] arr = Gen1DArr(5, 0.0, 1.0);
+
+// double result = DiffElem(arr);
+
+// Print1DArr(arr);
+// PrintData("Разница мин макс:", result);
+
+
+
+// Вариант со звездочкой
+
+
 int[] Gen1DArr(int len, int b, int e)
 {
     int[] arr = new int[len];
@@ -37,19 +88,35 @@ void PrintData(string msg1, int msg2)
     Console.WriteLine(msg2);
 }
 
-int DiffElem(int [] arr)
+int DiffElem(int[] intArray)
 {
     int res = 0;
-    res = arr.Max() - arr.Min();
+    int min = intArray[0];
+    int max = intArray[intArray.Length-1];
+    res = max-min;
     return res;
 }
 
-// bool Test(int n)
-// {
-//     return ((arr[i]%2!=0));
-// }
+
+
+int[] ArrSort(int [] arr)
+ 
+{
+    int[] intArray = arr;
+    Array.Sort(intArray);
+     for (int i = 0; i < arr.Length; i++)
+    {
+        intArray[i] = intArray[i]++;
+    }
+    
+       return intArray;
+    }
+
+
 
 int[] arr = Gen1DArr(5, 10, 99);
+Print1DArr(arr);
+ArrSort(arr);
 
 int result = DiffElem(arr);
 
