298dcdc491bdf855b5e7e7a93923e5d404bcf182
Denis Plotnikov <denis.plotnikov.82@ya.ru>
2023-02-15 16:38:23 +0500

    added tasks 46, 48

---
+++ Sem7Task46/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача №46
+// Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+// Например:
+// m = 3, n = 4.
+// -1 4 8 19
+// 5 -2 33 -2
+// -77 3 8 1
+
+ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+
+//1) Получение данных от пользователя
+int n = InputNum("Количество строк матрицы");
+int m = InputNum("Количество столбцов матрицы");
+
+//2) Генерация двумерного массива
+int[,] matrix = FillTwoDimArray(n, m);
+
+//3) Печатаем двумерный массив
+PrintTwoDimArray(matrix);
+
+int InputNum(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+
+int[,] FillTwoDimArray(int countRow, int countColumn)
+{
+    System.Random numberSyntezator = new System.Random();
+    int i = 0; int j = 0;
+    int[,] outArray = new int[countRow, countColumn];
+    while (i < countRow)
+    {
+        j = 0;
+        while (j < countColumn)
+        {
+            outArray[i, j] = numberSyntezator.Next(0, 101);
+            j++;
+        }
+        i++;
+    }
+    return outArray;
+}
+
+
+void PrintTwoDimArray(int[,] inputArray)
+{
+    int i = 0; int j = 0;
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            Console.ForegroundColor = col[new System.Random().Next(0,16)];
+            Console.Write(inputArray[i, j] + " ");
+            Console.ResetColor();
+            j++;
+        }
+        Console.WriteLine();
+        i++;
+    }
+}
+
---
+++ Sem7Task48/Program.cs
@@ -0,0 +1,68 @@
+﻿// Задача №48
+// Задайте двумерный массив размера m на n, каждый
+// элемент в массиве находится по формуле: Aₘₙ =
+// m+n. Выведите полученный массив на экран.
+// Пример:
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+ConsoleColor[] col = new ConsoleColor[]{ConsoleColor.Black,ConsoleColor.Blue,ConsoleColor.Cyan,
+                                        ConsoleColor.DarkBlue,ConsoleColor.DarkCyan,ConsoleColor.DarkGray,
+                                        ConsoleColor.DarkGreen,ConsoleColor.DarkMagenta,ConsoleColor.DarkRed,
+                                        ConsoleColor.DarkYellow,ConsoleColor.Gray,ConsoleColor.Green,
+                                        ConsoleColor.Magenta,ConsoleColor.Red,ConsoleColor.White,
+                                        ConsoleColor.Yellow};
+
+
+//1) Получение данных от пользователя
+int n = InputNum("Количество строк матрицы");
+int m = InputNum("Количество столбцов матрицы");
+
+//2) Генерация двумерного массива
+int[,] matrix = Gen2DArr(n, m);
+
+//3) Печатаем двумерный массив
+PrintTwoDimArray(matrix);
+
+int InputNum(string msg)
+{
+    Console.Write(msg);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+
+int[,] Gen2DArr(int row, int col)
+
+
+{
+    int[i] arr = new int[row,col];
+    for(int i=0;i<row;i++)
+    {
+        for(int j=0;i<col;j++)
+        {
+            arr[i,j]=i+j;
+        }
+    }
+    return arr;
+}
+
+
+void PrintTwoDimArray(int[,] inputArray)
+{
+    int i = 0; int j = 0;
+    while (i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while (j < inputArray.GetLength(1))
+        {
+            Console.ForegroundColor = col[new System.Random().Next(0,16)];
+            Console.Write(inputArray[i, j] + " ");
+            Console.ResetColor();
+            j++;
+        }
+        Console.WriteLine();
+        i++;
+    }
+}
