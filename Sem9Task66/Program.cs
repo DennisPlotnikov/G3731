fcab6c64a2708aef5664739f767766d37347f9e4
Denis Plotnikov <denis.plotnikov.82@ya.ru>
2023-02-21 22:11:45 +0500

    Added Tasks 64,66,68

---
+++ Sem9Task66/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу,
+// которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+
+// Чтение данных из консоли
+int ReadData(string line)
+{
+    Console.Write(line);
+    int number = int.Parse(Console.ReadLine() ?? "0");
+    return number;
+}
+
+// Печать результата
+void PrintResult(int prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+
+int RecLineMN(int m, int n)
+{
+    int sum = 0;
+    if (m >= n)
+    {
+        return n;
+
+    }
+    else
+    {
+        sum = m + RecLineMN(m + 1, n);
+           }
+    return sum;
+}
+
+int numN = ReadData("Введите число N: ");
+int numM = ReadData("Введите число M: ");
+int res = numM < numN ? RecLineMN(numM, numN) : RecLineMN(numN, numM);
+PrintResult(res);
---
+++ Sem9Task68/Program.cs
@@ -0,0 +1,54 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
+// Даны два неотрицательных числа m и n.
+// m = 2, n = 3 -> A(m,n) = 9
+// m = 3, n = 2 -> A(m,n) = 29
+
+int ReadData(string line)
+{
+    Console.Write(line);
+    return int.Parse(Console.ReadLine() ?? "0");
+}
+
+void PrintResult(long prefix)
+{
+    Console.WriteLine(prefix);
+}
+
+// функция ack(n, m)
+//    если n = 0
+//      вернуть m + 1
+//    иначе, если m = 0
+//      вернуть ack (n - 1, 1)
+//    еще
+//      вернуть ack(n - 1, ack (n, m - 1))
+
+
+long Ack(long a, long b)
+{
+    if (a == 0) return b + 1;
+    if (b == 0) return Ack(a - 1, 1);
+    else
+        return Ack(a - 1, Ack(a, b - 1));
+}
+
+
+// long MyPow(int a, int b)
+// {
+//     if(b==2)return a*a;
+//     return MyPow(a,b/2)*MyPow(a,b/2);
+// }
+
+int numberA = ReadData("Enter number A:");
+int numberB = ReadData("Enter number B:");
+
+// long res1 =0;
+// long res2 =0;
+
+// DateTime d1 = DateTime.Now;
+long res1 = Ack(numberA, numberB);
+// PrintResult("Решение Ack" + (DateTime.Now - d1));
+// ConsoleWriteLine();
+PrintResult(res1);
+// DateTime d2 = DateTime.Now;
+// res2 = MyPow(numberA,numberB);
+// PrintResult("Решение MyPow" + (DateTime.Now - d2));
